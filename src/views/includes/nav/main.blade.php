b38bea55eca2358c8440d45d13f36cb9f044b4fd
Jose Carlos <cpalacin@syscover.com>
2015-09-01 19:12:00 +0200

    font awesome

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -2,22 +2,22 @@
             <a href="javascript:void(0);"><i class="sys-icon-edit-write"></i>CMS</a>
             <ul class="sub-menu">
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-article', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-article') !!}><a href="{{ route('CmsArticle', [session('baseLang')]) }}"><i class="icon-file-text-alt"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-article') !!}><a href="{{ route('CmsArticle', [session('baseLang')]) }}"><i class="fa fa-file-text-alt"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-library', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-library') !!}><a href="{{ route('CmsLibrary') }}"><i class="icon-book"></i>{{ trans_choice('pulsar::pulsar.library', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-library') !!}><a href="{{ route('CmsLibrary') }}"><i class="fa fa-book"></i>{{ trans_choice('pulsar::pulsar.library', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-article-family', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-article-family') !!}><a href="{{ route('CmsArticleFamily') }}"><i class="icon-align-justify"></i>{{ trans_choice('cms::pulsar.article_family', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-article-family') !!}><a href="{{ route('CmsArticleFamily') }}"><i class="fa fa-align-justify"></i>{{ trans_choice('cms::pulsar.article_family', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-section', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('cms-section') !!}><a href="{{ route('CmsSection') }}"><i class="sys-icon-magnet"></i>{{ trans_choice('pulsar::pulsar.section', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-category', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-category') !!}><a href="{{ route('CmsCategory', [session('baseLang')]) }}"><i class="icon-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-category') !!}><a href="{{ route('CmsCategory', [session('baseLang')]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-attachment-family', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-attachment-family') !!}><a href="{{ route('CmsAttachmentFamily') }}"><i class="icon-picture"></i>{{ trans_choice('cms::pulsar.attachment_family', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-attachment-family') !!}><a href="{{ route('CmsAttachmentFamily') }}"><i class="fa fa-picture-o"></i>{{ trans_choice('cms::pulsar.attachment_family', 2) }}</a></li>
                 @endif
             </ul>
         </li>
