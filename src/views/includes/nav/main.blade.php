bdedce2cfdae7a770ab230f3247ba23ecafb54f6
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-11-29 15:04:23 +0100

    add suffix custom to avoid confits with other fields

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -4,14 +4,15 @@
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-article', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('cms-article') !!}><a href="{{ route('CmsArticle', [session('baseLang')]) }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
                 @endif
+                @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-category', 'access'))
+                    <li{!! Miscellaneous::setCurrentPage('cms-category') !!}><a href="{{ route('CmsCategory', [session('baseLang')]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
+                @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-section', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('cms-section') !!}><a href="{{ route('CmsSection') }}"><i class="sys-icon-magnet"></i>{{ trans_choice('pulsar::pulsar.section', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-article-family', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('cms-article-family') !!}><a href="{{ route('CmsArticleFamily') }}"><i class="fa fa-align-justify"></i>{{ trans_choice('cms::pulsar.article_family', 2) }}</a></li>
                 @endif
-                @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'cms-category', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-category') !!}><a href="{{ route('CmsCategory', [session('baseLang')]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
-                @endif
+
             </ul>
         </li>
