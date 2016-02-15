57d6069d82c5d9d8ed8ff0652d6da545f020c7f9
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-02-15 18:35:45 +0100

    fixed baseLang

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -2,10 +2,10 @@
             <a href="javascript:void(0)"><i class="sys-icon-edit-write"></i>CMS</a>
             <ul class="sub-menu">
                 @if(session('userAcl')->allows('cms-article', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-article') !!}><a href="{{ route('CmsArticle', [session('baseLang')]) }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-article') !!}><a href="{{ route('CmsArticle', [session('baseLang')->id_001]) }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->allows('cms-category', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('cms-category') !!}><a href="{{ route('CmsCategory', [session('baseLang')]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('cms-category') !!}><a href="{{ route('CmsCategory', [session('baseLang')->id_001]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->allows('cms-section', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('cms-section') !!}><a href="{{ route('CmsSection') }}"><i class="sys-icon-magnet"></i>{{ trans_choice('pulsar::pulsar.section', 2) }}</a></li>
