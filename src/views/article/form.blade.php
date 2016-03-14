62b1c869ec32a9095195c40d0cb5da563c32c1e0
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-14 18:50:44 +0100

    minor fix

--- src/views/article/form.blade.php
+++ src/views/article/form.blade.php
@@ -48,15 +48,80 @@
     <!-- cms::articles.create -->
     <div class="row">
         <div class="col-md-6">
-            @include('pulsar::includes.html.form_text_group', ['label' => 'ID', 'name' => 'id',  'value' => old('name', isset($object->id_355)? $object->id_355 : null), 'readOnly' => true, 'labelSize' => 4, 'fieldSize' => 4])
-            @include('pulsar::includes.html.form_image_group', ['label' => trans_choice('pulsar::pulsar.language', 1), 'name' => 'lang', 'nameImage' => $lang->name_001, 'value' => $lang->id_001, 'url' => asset('/packages/syscover/pulsar/storage/langs/' . $lang->image_001), 'labelSize' => 4, 'fieldSize' => 8])
-            @include('pulsar::includes.html.form_select_group', ['label' => trans_choice('pulsar::pulsar.section', 1), 'id' => 'section', 'name' => 'section', 'value' => old('section', isset($object->section_355)? $object->section_355 : null), 'objects' => $sections, 'idSelect' => 'id_350', 'nameSelect' => 'name_350', 'class' => 'select2', 'labelSize' => 4, 'fieldSize' => 8, 'required' => true, 'data' => ['language' => config('app.locale'), 'width' => '100%', 'error-placement' => 'select2-section-outer-container']])
-            @include('pulsar::includes.html.form_select_group', ['label' => trans_choice('pulsar::pulsar.family', 1), 'id' => 'family', 'name' => 'family', 'value' => old('family', isset($object->family_355)? $object->family_355 : null), 'objects' => $families, 'idSelect' => 'id_351', 'nameSelect' => 'name_351', 'class' => 'select2', 'labelSize' => 4, 'fieldSize' => 8, 'data' => ['language' => config('app.locale'), 'width' => '100%', 'error-placement' => 'select2-family-outer-container']])
+            @include('pulsar::includes.html.form_text_group', [
+                'label' => 'ID',
+                'name' => 'id',
+                'value' => old('name', isset($object->id_355)? $object->id_355 : null),
+                'readOnly' => true,
+                'labelSize' => 4,
+                'fieldSize' => 4
+            ])
+            @include('pulsar::includes.html.form_image_group', [
+                'label' => trans_choice('pulsar::pulsar.language', 1),
+                'name' => 'lang',
+                'nameImage' => $lang->name_001,
+                'value' => $lang->id_001,
+                'url' => asset('/packages/syscover/pulsar/storage/langs/' . $lang->image_001),
+                'labelSize' => 4,
+                'fieldSize' => 8
+            ])
+            @include('pulsar::includes.html.form_select_group', [
+                'label' => trans_choice('pulsar::pulsar.section', 1),
+                'id' => 'section',
+                'name' => 'section',
+                'value' => old('section', isset($object->section_355)? $object->section_355 : null),
+                'objects' => $sections,
+                'idSelect' => 'id_350',
+                'nameSelect' => 'name_350',
+                'class' => 'select2',
+                'labelSize' => 4,
+                'fieldSize' => 8,
+                'required' => true,
+                'data' => [
+                    'language' => config('app.locale'),
+                    'width' => '100%',
+                    'error-placement' => 'select2-section-outer-container'
+                ]
+            ])
+            @include('pulsar::includes.html.form_select_group', [
+                'label' => trans_choice('pulsar::pulsar.family', 1),
+                'id' => 'family',
+                'name' => 'family',
+                'value' => old('family', isset($object->family_355)? $object->family_355 : null),
+                'objects' => $families,
+                'idSelect' => 'id_351',
+                'nameSelect' => 'name_351',
+                'class' => 'select2',
+                'labelSize' => 4,
+                'fieldSize' => 8,
+                'data' => [
+                    'language' => config('app.locale'),
+                    'width' => '100%',
+                    'error-placement' => 'select2-family-outer-container'
+                ]
+            ])
         </div>
         <div class="col-md-6">
-            @include('pulsar::includes.html.form_text_group', ['label' => trans('cms::pulsar.author'), 'name' => 'authorName',  'value' =>  auth('pulsar')->user()->name_010 . ' ' . auth('pulsar')->user()->surname_010, 'readOnly' => true, 'labelSize' => 4, 'fieldSize' => 8])
+            @include('pulsar::includes.html.form_text_group', [
+                'label' => trans('cms::pulsar.author'),
+                'name' => 'authorName',
+                'value' =>  auth('pulsar')->user()->name_010 . ' ' . auth('pulsar')->user()->surname_010,
+                'readOnly' => true,
+                'labelSize' => 4,
+                'fieldSize' => 8
+            ])
             <input type="hidden" name="author" value="{{ auth('pulsar')->user()->id_010 }}">
-            @include('pulsar::includes.html.form_select_group', ['label' => trans('cms::pulsar.status'), 'name' => 'status', 'value' => old('status', isset($object->status_355)? $object->status_355 : null), 'objects' => $statuses, 'idSelect' => 'id', 'nameSelect' => 'name', 'labelSize' => 4, 'fieldSize' => 8, 'required' => true])
+            @include('pulsar::includes.html.form_select_group', [
+                'label' => trans('cms::pulsar.status'),
+                'name' => 'status',
+                'value' => old('status', isset($object->status_355)? $object->status_355 : null),
+                'objects' => $statuses,
+                'idSelect' => 'id',
+                'nameSelect' => 'name',
+                'labelSize' => 4,
+                'fieldSize' => 8,
+                'required' => true
+            ])
             @include('pulsar::includes.html.form_datetimepicker_group', [
                 'label' => trans('cms::pulsar.publish'),
                 'name' => 'publish',
@@ -76,10 +141,53 @@
         'icon' => 'fa fa-inbox',
         'containerId' => 'headerContent'
     ])
-    @include('pulsar::includes.html.form_datetimepicker_group', ['label' => trans_choice('pulsar::pulsar.date', 1), 'containerId' => 'dateContent', 'name' => 'date', 'id' => 'idDate', 'value' => old('date', isset($object->date_355)? date(config('pulsar.datePattern'), $object->date_355) : date(config('pulsar.datePattern'))), 'required' => true, 'fieldSize' => 4, 'data' => ['format' => Miscellaneous::convertFormatDate(config('pulsar.datePattern')), 'locale' => config('app.locale')]])
-    @include('pulsar::includes.html.form_text_group', ['label' => trans('pulsar::pulsar.title'), 'containerId' => 'titleContent', 'name' => 'title', 'value' => old('title', isset($object->title_355)? $object->title_355 : null), 'maxLength' => '510', 'rangeLength' => '2,510', 'required' => true])
-    @include('pulsar::includes.html.form_text_group', ['label' => trans('pulsar::pulsar.slug'), 'containerId' => 'slugContent', 'name' => 'slug', 'value' => old('slug', isset($object->slug_355)? $object->slug_355 : null), 'maxLength' => '255', 'rangeLength' => '2,255', 'required' => true])
-    @include('pulsar::includes.html.form_select_group', ['label' => trans_choice('pulsar::pulsar.category', 2), 'containerId' => 'categoriesContent', 'name' => 'categories[]', 'value' => old('categories', isset($object)? $object->getCategories : null), 'objects' => $categories, 'idSelect' => 'id_352', 'nameSelect' => 'name_352', 'multiple' => true, 'class' => 'col-md-12 select2', 'fieldSize' => 10, 'data' => ['placeholder' => trans('pulsar::pulsar.select_category'), 'width' => '100%']])
+    @include('pulsar::includes.html.form_datetimepicker_group', [
+        'label' => trans_choice('pulsar::pulsar.date', 1),
+        'containerId' => 'dateContent',
+        'name' => 'date',
+        'id' => 'idDate',
+        'value' => old('date', isset($object->date_355)? date(config('pulsar.datePattern'), $object->date_355) : date(config('pulsar.datePattern'))),
+        'required' => true,
+        'fieldSize' => 4,
+        'data' => [
+            'format' => Miscellaneous::convertFormatDate(config('pulsar.datePattern')),
+            'locale' => config('app.locale')
+        ]
+    ])
+    @include('pulsar::includes.html.form_text_group', [
+        'label' => trans('pulsar::pulsar.title'),
+        'containerId' => 'titleContent',
+        'name' => 'title',
+        'value' => old('title', isset($object->title_355)? $object->title_355 : null),
+        'maxLength' => '510',
+        'rangeLength' => '2,510',
+        'required' => true
+    ])
+    @include('pulsar::includes.html.form_text_group', [
+        'label' => trans('pulsar::pulsar.slug'),
+        'containerId' => 'slugContent',
+        'name' => 'slug',
+        'value' => old('slug', isset($object->slug_355)? $object->slug_355 : null),
+        'maxLength' => '255',
+        'rangeLength' => '2,255',
+        'required' => true
+    ])
+    @include('pulsar::includes.html.form_select_group', [
+        'label' => trans_choice('pulsar::pulsar.category', 2),
+        'containerId' => 'categoriesContent',
+        'name' => 'categories[]',
+        'value' => old('categories', isset($object)? $object->getCategories : null),
+        'objects' => $categories,
+        'idSelect' => 'id_352',
+        'nameSelect' => 'name_352',
+        'multiple' => true,
+        'class' => 'col-md-12 select2',
+        'fieldSize' => 10,
+        'data' => [
+            'placeholder' => trans('pulsar::pulsar.select_category'),
+            'width' => '100%'
+        ]
+    ])
     @include('pulsar::includes.html.form_text_group', [
         'fieldSize' => 6,
         'label' => trans('pulsar::pulsar.link'),
