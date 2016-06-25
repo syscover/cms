df739c622f7d33ee9b83b65896ae9f07a896d84a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-06-25 18:49:32 +0200

    change package column name

--- src/database/seeds/CmsResourceTableSeeder.php
+++ src/database/seeds/CmsResourceTableSeeder.php
@@ -8,11 +8,11 @@ class CmsResourceTableSeeder extends Seeder {
     public function run()
     {
         Resource::insert([
-            ['id_007' => 'cms',                 'name_007' => 'CMS Package',        'package_007' => '13'],
-            ['id_007' => 'cms-article',         'name_007' => 'Articles',           'package_007' => '13'],
-            ['id_007' => 'cms-article-family',  'name_007' => 'Article families',   'package_007' => '13'],
-            ['id_007' => 'cms-category',        'name_007' => 'Categories',         'package_007' => '13'],
-            ['id_007' => 'cms-section',         'name_007' => 'Sections',           'package_007' => '13']
+            ['id_007' => 'cms',                 'name_007' => 'CMS Package',        'package_id_007' => '13'],
+            ['id_007' => 'cms-article',         'name_007' => 'Articles',           'package_id_007' => '13'],
+            ['id_007' => 'cms-article-family',  'name_007' => 'Article families',   'package_id_007' => '13'],
+            ['id_007' => 'cms-category',        'name_007' => 'Categories',         'package_id_007' => '13'],
+            ['id_007' => 'cms-section',         'name_007' => 'Sections',           'package_id_007' => '13']
         ]);
     }
 }
