35d1d6714da217c1beab7f64beffcfcb682077af
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-08-08 13:00:05 +0200

    set helper base_lang()

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -2,10 +2,10 @@
     <a href="javascript:void(0)"><i class="sys-icon-edit-write"></i>CMS</a>
     <ul class="sub-menu">
         @if(is_allowed('cms-article', 'access'))
-            <li{!! is_current_resource('cms-article') !!}><a href="{{ route('cmsArticle', [session('baseLang')->id_001]) }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
+            <li{!! is_current_resource('cms-article') !!}><a href="{{ route('cmsArticle', [base_lang()->id_001]) }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.article', 2) }}</a></li>
         @endif
         @if(is_allowed('cms-category', 'access'))
-            <li{!! is_current_resource('cms-category') !!}><a href="{{ route('cmsCategory', [session('baseLang')->id_001]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
+            <li{!! is_current_resource('cms-category') !!}><a href="{{ route('cmsCategory', [base_lang()->id_001]) }}"><i class="fa fa-list-ol"></i>{{ trans_choice('pulsar::pulsar.category', 2) }}</a></li>
         @endif
         @if(is_allowed('cms-section', 'access'))
             <li{!! is_current_resource('cms-section') !!}><a href="{{ route('cmsSection') }}"><i class="sys-icon-magnet"></i>{{ trans_choice('pulsar::pulsar.section', 2) }}</a></li>
