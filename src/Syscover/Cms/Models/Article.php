f9588ba8fe88ff8d36a99f03a0348782337bdadc
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-09-14 13:29:38 +0200

    minor fix

--- src/Syscover/Cms/Models/Article.php
+++ src/Syscover/Cms/Models/Article.php
@@ -41,7 +41,7 @@ class Article extends Model
     {
         if(isset($specialRules['slugRule']) && $specialRules['slugRule'])
         {
-            //static::$rules['slug'] = 'mysql2.unique:013_355_article,slug_355,NULL,013_355_article,lang_id_355,' . $data['lang'];
+            //static::$rules['slug'] = 'unique:mysql2.013_355_article,slug_355,NULL,013_355_article,lang_id_355,' . $data['lang'];
             static::$rules['slug'] =
                 Rule::unique('mysql2.013_355_article', 'slug_355')
                     ->ignore($data['id'], 'id_355');
